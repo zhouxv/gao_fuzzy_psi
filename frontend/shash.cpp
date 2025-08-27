5c8f444a5c015d472299b03b1d8bee4a5c5949dd
zhouxv <1598284508@qq.com>
2025-08-27 10:38:33 +0800

    去掉shash encoding的时间

--- frontend/shash.cpp
+++ frontend/shash.cpp
@@ -21,7 +21,7 @@ namespace osuCrypto {
 
 std::pair<double, double> test_sas_fm(const CLP &cmd) {
   const u64 dimension = cmd.getOr("d", 2);
-  const u64 delta = cmd.getOr("delta", 16);
+  const u64 delta = cmd.getOr("delta", 10);
   const u64 side_length = 1;
   const u64 p = 0;
   const u64 recv_set_size = 1ull << cmd.getOr("r", 8);
@@ -67,7 +67,7 @@ std::pair<double, double> test_sas_fm(const CLP &cmd) {
           ((i8)((prng.get<u8>()) % (delta - 1)) - delta / 2);
     }
   }
-  // std::cout << "data init done" << std::endl;
+  std::cout << "data init done" << std::endl;
 
   ///////////////////////////////////////////////////////////////////////////////////////
   // key generate
@@ -88,6 +88,8 @@ std::pair<double, double> test_sas_fm(const CLP &cmd) {
   ipcl::initializeContext("QAT");
   ipcl::KeyPair paillier_key = ipcl::generateKeypair(2048, true);
   ipcl::setHybridMode(ipcl::HybridMode::OPTIMAL);
+
+  std::cout << "key gen done" << std::endl;
 
   Timer time;
 
@@ -116,7 +118,165 @@ std::pair<double, double> test_sas_fm(const CLP &cmd) {
   std::vector<Rist25519_number> send_masks;
   std::vector<Rist25519_number> send_masks_inv;
   fmap::get_mask_cipher(send_set_size, send_masks, send_masks_inv, send_pk);
-  // std::cout << "fmap offline done" << std::endl;
+
+  std::vector<Rist25519_point> recv_seeds;
+  std::vector<Rist25519_point> sender_seeds;
+  u64 fmath_recv_size;
+  u64 fmath_sender_size;
+
+  auto a = fmap::fmap_recv_encode_offline(
+      &receiver_elements, &recv_values, &recv_vals_candidate_r,
+      &recv_vals_candidate_skr, dimension, delta, side_length, recv_seeds,
+      fmath_recv_size);
+
+  auto b = fmap::fmap_send_encode_offline(
+      &sender_elements, &send_values, &send_vals_candidate_r,
+      &send_vals_candidate_skr, dimension, delta, side_length, sender_seeds,
+      fmath_sender_size);
+
+  std::cout << "fmap offline done" << std::endl;
+
+  time.setTimePoint("offline");
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // online
+  ///////////////////////////////////////////////////////////////////////////////////////
+
+  tStart(online_time);
+
+  auto sockets = coproto::LocalAsyncSocket::makePair();
+
+  std::vector<Rist25519_point> recv_vec_dhkk_seedsum(recv_set_size);
+  std::vector<Rist25519_point> send_vec_dhkk_seedsum(send_set_size);
+
+  std::thread thread_fmap_recv(
+      fmap::fmap_recv_online2, &sockets[0], &receiver_elements, &recv_masks,
+      &recv_masks_inv, &recv_vec_dhkk_seedsum, dimension, delta, side_length,
+      recv_sk, recv_pk, recv_dh_sk, &a, &recv_seeds, fmath_recv_size);
+  std::thread thread_fmap_send(
+      fmap::fmap_send_online2, &sockets[1], &sender_elements, &send_masks,
+      &send_masks_inv, &send_vec_dhkk_seedsum, dimension, delta, side_length,
+      send_sk, send_pk, send_dh_sk, &b, &sender_seeds, fmath_sender_size);
+
+  thread_fmap_recv.join();
+  thread_fmap_send.join();
+  double fmap_online_time = tEnd(online_time);
+
+  auto recv_bytes_present_fmap = sockets[0].bytesSent();
+  auto send_bytes_present_fmap = sockets[1].bytesSent();
+
+  time.setTimePoint("fmap done");
+
+  time.setTimePoint("online done");
+
+  auto recv_bytes_present = sockets[0].bytesSent();
+  auto send_bytes_present = sockets[1].bytesSent();
+
+  return {fmap_online_time,
+          ((recv_bytes_present) + (send_bytes_present)) / 1024.0 / 1024.0};
+}
+
+std::pair<double, double> test_sas_fm_origin(const CLP &cmd) {
+  const u64 dimension = cmd.getOr("d", 2);
+  const u64 delta = cmd.getOr("delta", 10);
+  const u64 side_length = 1;
+  const u64 p = 0;
+  const u64 recv_set_size = 1ull << cmd.getOr("r", 8);
+  const u64 send_set_size = 1ull << cmd.getOr("s", 8);
+  const u64 intersection_size = cmd.getOr("i", 10);
+  if ((intersection_size > recv_set_size) |
+      (intersection_size > send_set_size)) {
+    throw std::runtime_error(
+        "intersection_size should not be greater than set_size");
+  }
+
+  PRNG prng(oc::sysRandomSeed());
+
+  std::vector<std::vector<u64>> receiver_elements(
+      recv_set_size, std::vector<u64>(dimension, 0));
+  std::vector<std::vector<u64>> sender_elements(send_set_size,
+                                                std::vector<u64>(dimension, 0));
+
+  for (u64 i = 0; i < recv_set_size; i++) {
+    for (u64 j = 0; j < dimension; j++) {
+      receiver_elements[i][j] =
+          (prng.get<u64>()) % ((0xffff'ffff'ffff'ffff) - 3 * delta) +
+          1.5 * delta;
+    }
+  }
+
+  for (u64 i = 0; i < send_set_size; i++) {
+    for (u64 j = 0; j < dimension; j++) {
+      sender_elements[i][j] =
+          (prng.get<u64>()) % ((0xffff'ffff'ffff'ffff) - 3 * delta) +
+          1.5 * delta;
+    }
+  }
+
+  u64 base_pos = (prng.get<u64>()) % (send_set_size - intersection_size - 1);
+  // u64 base_pos = 0;
+  for (u64 i = base_pos; i < base_pos + intersection_size; i++) {
+    for (u64 j = 0; j < dimension; j++) {
+      sender_elements[i][j] = receiver_elements[i - base_pos][j];
+    }
+    for (u64 j = 0; j < 2; j++) {
+      sender_elements[i][j] +=
+          ((i8)((prng.get<u8>()) % (delta - 1)) - delta / 2);
+    }
+  }
+  std::cout << "data init done" << std::endl;
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // key generate
+  ///////////////////////////////////////////////////////////////////////////////////////
+  Rist25519_number recv_sk(prng);
+  std::array<Rist25519_point, 2> recv_pk;
+  recv_pk[0] = Rist25519_point(prng);
+  recv_pk[1] = recv_sk * recv_pk[0];
+
+  Rist25519_number send_sk(prng);
+  std::array<Rist25519_point, 2> send_pk;
+  send_pk[0] = Rist25519_point(prng);
+  send_pk[1] = send_sk * send_pk[0];
+
+  Rist25519_number recv_dh_sk(prng);
+  Rist25519_number send_dh_sk(prng);
+
+  ipcl::initializeContext("QAT");
+  ipcl::KeyPair paillier_key = ipcl::generateKeypair(2048, true);
+  ipcl::setHybridMode(ipcl::HybridMode::OPTIMAL);
+
+  std::cout << "key gen done" << std::endl;
+
+  Timer time;
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // offline
+  ///////////////////////////////////////////////////////////////////////////////////////
+  time.setTimePoint("Start");
+
+  tVar online_time;
+
+  std::stack<Rist25519_number> recv_vals_candidate_r;
+  std::stack<Rist25519_number> recv_vals_candidate_skr;
+  std::vector<std::vector<Rist25519_number>> recv_values;
+  fmap::assign_segments(recv_set_size, recv_values, recv_vals_candidate_r,
+                        recv_vals_candidate_skr, dimension, delta, side_length,
+                        recv_sk);
+  std::stack<Rist25519_number> send_vals_candidate_r;
+  std::stack<Rist25519_number> send_vals_candidate_skr;
+  std::vector<std::vector<Rist25519_number>> send_values;
+  fmap::assign_segments(send_set_size, send_values, send_vals_candidate_r,
+                        send_vals_candidate_skr, dimension, delta, side_length,
+                        send_sk);
+  std::vector<Rist25519_number> recv_masks;
+  std::vector<Rist25519_number> recv_masks_inv;
+  fmap::get_mask_cipher(recv_set_size, recv_masks, recv_masks_inv, recv_pk);
+  std::vector<Rist25519_number> send_masks;
+  std::vector<Rist25519_number> send_masks_inv;
+  fmap::get_mask_cipher(send_set_size, send_masks, send_masks_inv, send_pk);
+
+  std::cout << "fmap offline done" << std::endl;
 
   time.setTimePoint("offline");
 
@@ -170,6 +330,7 @@ void test_shash(const CLP &cmd) {
   const u64 send_set_size = 1ull << cmd.getOr("s", 8);
   const u64 intersection_size = cmd.getOr("i", 10);
   const u64 trait = cmd.getOr("trait", 1);
+  const bool origin = cmd.isSet("origin");
   if ((intersection_size > recv_set_size) |
       (intersection_size > send_set_size)) {
     throw std::runtime_error("intersection_size > set_size");
@@ -183,7 +344,7 @@ void test_shash(const CLP &cmd) {
 
   std::vector<double> times(trait), comus(trait);
   for (u64 i = 0; i < trait; i++) {
-    auto tmp = test_sas_fm(cmd);
+    auto tmp = origin ? test_sas_fm_origin(cmd) : test_sas_fm(cmd);
     times[i] = tmp.first;
     comus[i] = tmp.second;
   }
--- fuzzy_mapping/fuzzy_mapping.cpp
+++ fuzzy_mapping/fuzzy_mapping.cpp
@@ -142,10 +142,13 @@ void assign_segments(const std::size_t &elements_size,
   Rist25519_number value_i = Rist25519_number(prng);
   Rist25519_number rG_value = Rist25519_number(prng);
   Rist25519_number sk_rG_value = sk * rG_value;
+
+  std::vector<Rist25519_number> values_i(elements_size, value_i);
+
   for (auto i = 0; i < dimension; i++) {
-    std::vector<Rist25519_number> values_i;
+
     for (auto j = 0; j < elements_size; j++) {
-      values_i.push_back(value_i);
+      // values_i.push_back(value_i);
       //   values_i.push_back(Rist25519_number(prng));
       for (auto k = 0; k < num_interval_per_element_per_dimension; k++) {
         // rG = Rist25519_number(prng);
@@ -236,11 +239,6 @@ void get_key_value_pair(
       for (u64 k = segments_in_dimensions[i][j][0];
            k < segments_in_dimensions[i][j][1] + 1; k++) {
         keys.push_back(block(i, k));
-        // point_to_block_vector(vals_candidate_r.top(),
-        // temp_block_vector.begin());
-        // point_to_block_vector(vals_candidate_skr.top() + values[i][j],
-        // temp_block_vector.begin() + 2);
-        // point_to_block_vector( values[i][j], temp_block_vector.begin() + 2);
         temp_Rist25519_number_vector[0] = vals_candidate_r.top();
         temp_Rist25519_number_vector[1] =
             vals_candidate_skr.top() + values[i][j];
@@ -321,6 +319,249 @@ void get_dhkk_seedsum(const std::vector<Rist25519_point> &dhk_mask_seedsum,
   return;
 }
 
+/////////////////////////////////////////////////////////////////////////
+std::vector<Rist25519_point> fmap_recv_encode_offline(
+    std::vector<std::vector<u64>> *receiver_elements,
+    std::vector<std::vector<Rist25519_number>> *recv_values,
+    std::stack<Rist25519_number> *recv_vals_candidate_r,
+    std::stack<Rist25519_number> *recv_vals_candidate_skr, u32 dimension,
+    i32 delta, i32 side_length, std::vector<Rist25519_point> &recv_seeds,
+    u64 &fmath_recv_size) {
+  u64 recv_set_size((*receiver_elements).size());
+
+  std::vector<std::vector<segment>> recv_segments_in_dimensions;
+  fmap::elements_to_segments(*receiver_elements, recv_segments_in_dimensions,
+                             dimension, delta, side_length);
+
+  fmap::get_own_seeds(*receiver_elements, recv_segments_in_dimensions,
+                      *recv_values, recv_seeds, dimension, side_length);
+
+  std::vector<element> recv_fmap_keys;
+  std::vector<std::vector<Rist25519_number>> recv_fmap_vals;
+  fmap::get_key_value_pair(recv_segments_in_dimensions, *recv_values,
+                           *recv_vals_candidate_r, *recv_vals_candidate_skr,
+                           recv_fmap_keys, recv_fmap_vals, dimension);
+
+  RBOKVS_rist recv_fmap_okvs;
+  recv_fmap_okvs.init(recv_fmap_keys.size(), 0.1, lambda, seed);
+  std::vector<std::vector<Rist25519_point>> recv_fmap_codeWords(
+      recv_fmap_okvs.num_columns,
+      std::vector<Rist25519_point>(EC_CIPHER_SIZE_IN_NUMBER));
+  // recv_fmap_okvs.encode(recv_fmap_keys, recv_fmap_vals,
+  // EC_CIPHER_SIZE_IN_NUMBER, recv_fmap_codeWords,
+  // Rist25519_point::mulGenerator(1));
+
+  // recv_fmap_okvs.encode(recv_fmap_keys, recv_fmap_vals,
+  //                       EC_CIPHER_SIZE_IN_NUMBER, recv_fmap_codeWords);
+
+  std::vector<Rist25519_point> recv_fmap_codeWords_net(
+      recv_fmap_okvs.num_columns * EC_CIPHER_SIZE_IN_NUMBER);
+
+  for (u64 i = 0; i < recv_fmap_okvs.num_columns; i++) {
+    for (u64 j = 0; j < EC_CIPHER_SIZE_IN_NUMBER; j++) {
+      recv_fmap_codeWords_net[i * EC_CIPHER_SIZE_IN_NUMBER + j] =
+          recv_fmap_codeWords[i][j];
+    }
+  }
+
+  fmath_recv_size = recv_fmap_keys.size();
+  return recv_fmap_codeWords_net;
+}
+
+std::vector<Rist25519_point> fmap_send_encode_offline(
+    std::vector<std::vector<u64>> *sender_elements,
+    std::vector<std::vector<Rist25519_number>> *send_values,
+    std::stack<Rist25519_number> *send_vals_candidate_r,
+    std::stack<Rist25519_number> *send_vals_candidate_skr, u32 dimension,
+    i32 delta, i32 side_length, std::vector<Rist25519_point> &send_seeds,
+    u64 &fmath_sender_size) {
+
+  std::vector<std::vector<segment>> send_segments_in_dimensions;
+  fmap::elements_to_segments(*sender_elements, send_segments_in_dimensions,
+                             dimension, delta, side_length);
+
+  fmap::get_own_seeds(*sender_elements, send_segments_in_dimensions,
+                      *send_values, send_seeds, dimension, side_length);
+
+  std::vector<element> send_fmap_keys;
+  std::vector<std::vector<Rist25519_number>> send_fmap_vals;
+  fmap::get_key_value_pair(send_segments_in_dimensions, *send_values,
+                           *send_vals_candidate_r, *send_vals_candidate_skr,
+                           send_fmap_keys, send_fmap_vals, dimension);
+
+  RBOKVS_rist send_fmap_okvs;
+  send_fmap_okvs.init(send_fmap_keys.size(), 0.1, lambda, seed);
+  std::vector<std::vector<Rist25519_point>> send_fmap_codeWords(
+      send_fmap_okvs.num_columns,
+      std::vector<Rist25519_point>(EC_CIPHER_SIZE_IN_NUMBER));
+  // send_fmap_okvs.encode(send_fmap_keys, send_fmap_vals,
+  // EC_CIPHER_SIZE_IN_NUMBER, send_fmap_codeWords,
+  // Rist25519_point::mulGenerator(1));
+
+  // send_fmap_okvs.encode(send_fmap_keys, send_fmap_vals,
+  //                       EC_CIPHER_SIZE_IN_NUMBER, send_fmap_codeWords);
+
+  std::vector<Rist25519_point> send_fmap_codeWords_net(
+      send_fmap_okvs.num_columns * EC_CIPHER_SIZE_IN_NUMBER);
+  std::vector<Rist25519_point> recv_fmap_codeWords_net;
+
+  for (u64 i = 0; i < send_fmap_okvs.num_columns; i++) {
+    for (u64 j = 0; j < EC_CIPHER_SIZE_IN_NUMBER; j++) {
+      send_fmap_codeWords_net[i * EC_CIPHER_SIZE_IN_NUMBER + j] =
+          send_fmap_codeWords[i][j];
+    }
+  }
+
+  fmath_sender_size = send_fmap_keys.size();
+
+  return send_fmap_codeWords_net;
+}
+
+void fmap_recv_online2(coproto::LocalAsyncSocket *channel,
+                       std::vector<std::vector<u64>> *receiver_elements,
+                       std::vector<Rist25519_number> *recv_masks,
+                       std::vector<Rist25519_number> *recv_masks_inv,
+                       std::vector<Rist25519_point> *recv_vec_dhkk_seedsum,
+                       u32 dimension, i32 delta, i32 side_length,
+                       Rist25519_number recv_sk,
+                       std::array<Rist25519_point, 2> recv_pk,
+                       Rist25519_number recv_dh_sk,
+                       std::vector<Rist25519_point> *recv_fmap_codeWords_net,
+                       std::vector<Rist25519_point> *recv_seeds,
+                       u64 fmath_recv_size) {
+  u64 recv_set_size((*receiver_elements).size());
+
+  std::vector<Rist25519_point> send_fmap_codeWords_net;
+
+  // std::cout << "fmap_recv_online: fmap_codeWords send recv begin" <<
+  // std::endl;
+  coproto::sync_wait((*channel).send(*recv_fmap_codeWords_net));
+  // std::cout << "fmap_recv_online: fmap_codeWords send done" << std::endl;
+  coproto::sync_wait((*channel).recvResize(send_fmap_codeWords_net));
+  // std::cout << "fmap_recv_online: fmap_codeWords send recv done" <<
+  // std::endl;
+
+  std::vector<std::vector<Rist25519_point>> send_fmap_codeWords(
+      (send_fmap_codeWords_net.size() / EC_CIPHER_SIZE_IN_NUMBER),
+      std::vector<Rist25519_point>(EC_CIPHER_SIZE_IN_NUMBER));
+  for (u64 i = 0;
+       i < (send_fmap_codeWords_net.size() / EC_CIPHER_SIZE_IN_NUMBER); i++) {
+    for (u64 j = 0; j < EC_CIPHER_SIZE_IN_NUMBER; j++) {
+      send_fmap_codeWords[i][j] =
+          send_fmap_codeWords_net[i * EC_CIPHER_SIZE_IN_NUMBER + j];
+    }
+  }
+
+  u64 send_fmap_keys_size;
+  // std::cout << "fmap_recv_online: recv_fmap_keys send recv begin" <<
+  // std::endl;
+  coproto::sync_wait((*channel).send(fmath_recv_size));
+  // std::cout << "fmap_recv_online: recv_fmap_keys send done" << std::endl;
+  coproto::sync_wait((*channel).recvResize(send_fmap_keys_size));
+  // std::cout << "fmap_recv_online: recv_fmap_keys send recv done" <<
+  // std::endl;
+
+  std::array<Rist25519_point, 2> send_pk;
+  // std::cout << "fmap_recv_online: recv_pk send recv begin" << std::endl;
+  coproto::sync_wait((*channel).send(recv_pk));
+  // std::cout << "fmap_recv_online: recv_pk send done" << std::endl;
+  coproto::sync_wait((*channel).recvResize(send_pk));
+  // std::cout << "fmap_recv_online: recv_pk send recv done" << std::endl;
+
+  std::vector<Rist25519_point> recv_vec_enc_mask_seedsum;
+  fmap::get_vec_enc_mask_seedsum(*receiver_elements, send_fmap_codeWords,
+                                 *recv_masks, *recv_seeds,
+                                 recv_vec_enc_mask_seedsum, dimension,
+                                 side_length, send_fmap_keys_size, send_pk);
+
+  std::vector<Rist25519_point> send_vec_enc_mask_seedsum;
+  // std::cout << "fmap_recv_online: vec_enc_mask_seedsum send recv begin" <<
+  // std::endl;
+  coproto::sync_wait((*channel).send(recv_vec_enc_mask_seedsum));
+  // std::cout << "fmap_recv_online: vec_enc_mask_seedsum send done" <<
+  // std::endl;
+  coproto::sync_wait((*channel).recvResize(send_vec_enc_mask_seedsum));
+  // std::cout << "fmap_recv_online: vec_enc_mask_seedsum send recv done" <<
+  // std::endl;
+
+  std::vector<Rist25519_point> send_dhk_mask_seedsum;
+  fmap::get_dhk_mask_seedsum(send_vec_enc_mask_seedsum, send_dhk_mask_seedsum,
+                             recv_sk, recv_dh_sk);
+
+  std::vector<Rist25519_point> recv_dhk_mask_seedsum;
+  // std::cout << "fmap_recv_online: dhk_mask_seedsum send recv begin" <<
+  // std::endl;
+  coproto::sync_wait((*channel).send(send_dhk_mask_seedsum));
+  // std::cout << "fmap_recv_online: dhk_mask_seedsum send done" << std::endl;
+  coproto::sync_wait((*channel).recvResize(recv_dhk_mask_seedsum));
+  // std::cout << "fmap_recv_online: dhk_mask_seedsum send recv done" <<
+  // std::endl;
+  fmap::get_dhkk_seedsum(recv_dhk_mask_seedsum, *recv_masks_inv,
+                         *recv_vec_dhkk_seedsum, recv_dh_sk);
+
+  return;
+}
+
+void fmap_send_online2(coproto::LocalAsyncSocket *channel,
+                       std::vector<std::vector<u64>> *sender_elements,
+                       std::vector<Rist25519_number> *send_masks,
+                       std::vector<Rist25519_number> *send_masks_inv,
+                       std::vector<Rist25519_point> *send_vec_dhkk_seedsum,
+                       u32 dimension, i32 delta, i32 side_length,
+                       Rist25519_number send_sk,
+                       std::array<Rist25519_point, 2> send_pk,
+                       Rist25519_number send_dh_sk,
+                       std::vector<Rist25519_point> *send_fmap_codeWords_net,
+                       std::vector<Rist25519_point> *send_seeds,
+                       u64 fmath_sender_size) {
+
+  std::vector<Rist25519_point> recv_fmap_codeWords_net;
+
+  coproto::sync_wait((*channel).recvResize(recv_fmap_codeWords_net));
+  coproto::sync_wait((*channel).send(*send_fmap_codeWords_net));
+
+  std::vector<std::vector<Rist25519_point>> recv_fmap_codeWords(
+      (recv_fmap_codeWords_net.size() / EC_CIPHER_SIZE_IN_NUMBER),
+      std::vector<Rist25519_point>(EC_CIPHER_SIZE_IN_NUMBER));
+  for (u64 i = 0;
+       i < (recv_fmap_codeWords_net.size() / EC_CIPHER_SIZE_IN_NUMBER); i++) {
+    for (u64 j = 0; j < EC_CIPHER_SIZE_IN_NUMBER; j++) {
+      recv_fmap_codeWords[i][j] =
+          recv_fmap_codeWords_net[i * EC_CIPHER_SIZE_IN_NUMBER + j];
+    }
+  }
+
+  u64 recv_fmap_keys_size;
+  coproto::sync_wait((*channel).recvResize(recv_fmap_keys_size));
+  coproto::sync_wait((*channel).send(fmath_sender_size));
+
+  std::array<Rist25519_point, 2> recv_pk;
+  coproto::sync_wait((*channel).recvResize(recv_pk));
+  coproto::sync_wait((*channel).send(send_pk));
+
+  std::vector<Rist25519_point> send_vec_enc_mask_seedsum;
+  fmap::get_vec_enc_mask_seedsum(*sender_elements, recv_fmap_codeWords,
+                                 *send_masks, *send_seeds,
+                                 send_vec_enc_mask_seedsum, dimension,
+                                 side_length, recv_fmap_keys_size, recv_pk);
+
+  std::vector<Rist25519_point> recv_vec_enc_mask_seedsum;
+  coproto::sync_wait((*channel).recvResize(recv_vec_enc_mask_seedsum));
+  coproto::sync_wait((*channel).send(send_vec_enc_mask_seedsum));
+
+  std::vector<Rist25519_point> recv_dhk_mask_seedsum;
+  fmap::get_dhk_mask_seedsum(recv_vec_enc_mask_seedsum, recv_dhk_mask_seedsum,
+                             send_sk, send_dh_sk);
+
+  std::vector<Rist25519_point> send_dhk_mask_seedsum;
+  coproto::sync_wait((*channel).recvResize(send_dhk_mask_seedsum));
+  coproto::sync_wait((*channel).send(recv_dhk_mask_seedsum));
+  fmap::get_dhkk_seedsum(send_dhk_mask_seedsum, *send_masks_inv,
+                         *send_vec_dhkk_seedsum, send_dh_sk);
+
+  return;
+}
+
 ///////////////////////////////////////////////////////////////////////
 void fmap_recv_online(coproto::LocalAsyncSocket *channel,
                       std::vector<std::vector<u64>> *receiver_elements,
@@ -371,6 +612,7 @@ void fmap_recv_online(coproto::LocalAsyncSocket *channel,
           recv_fmap_codeWords[i][j];
     }
   }
+
   // std::cout << "fmap_recv_online: fmap_codeWords send recv begin" <<
   // std::endl;
   coproto::sync_wait((*channel).send(recv_fmap_codeWords_net));
